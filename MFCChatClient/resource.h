4ed38b42640618cdd0f894023e71167508550aeb
Airis <804194657@qq.com>
2024-03-31 15:12:46 +0800

    上传基线代码

---
+++ MFCChatClient/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatClient.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATCLIENT_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_SEND_BTN                    1000
+#define IDC_CLEARMSG_BTN                1001
+#define IDC_CONNECT_BTN                 1002
+#define IDC_DISCONNECT_BTN              1003
+#define IDC_SENDMSG_EDIT                1005
+#define IDC_AUTOSENDMSG_EDIT            1006
+#define IDC_PORT_EDIT                   1007
+#define IDC_AUTOSEND_RADIO              1008
+#define IDC_COLOUR_COMBO                1009
+#define IDC_IPADDRESS                   1010
+#define IDC_LIST1                       1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
